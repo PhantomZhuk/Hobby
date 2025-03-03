7609f1725746a753b35f1bbf7830b93cb3448beb
PhantomZhuk <bitmaksum550@gmail.com>
2025-03-03 22:59:41 +0200

    add authentication page and no page found (only structure without styles)

--- src/Page/NotFound/NotFound.tsx
+++ src/Page/NotFound/NotFound.tsx
@@ -1,7 +1,7 @@
 function NotFound() {
     return (
         <div>
-
+            <h1>404 Not Found</h1>
         </div>
     );
 }
--- src/components/AuthButton/AuthButton.tsx
+++ src/components/AuthButton/AuthButton.tsx
@@ -1,17 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 function AuthButton() {
+    const [isAuth, setIsAuth] = useState(false);
 
-    return (
-        <div>
+    useEffect(() => {
+        axios.get('http://localhost:5000/user/verifyToken', { withCredentials: true }).then(res => {
+            if (res.status === 200) {
+                setIsAuth(true);
+            } else {
+                setIsAuth(false);
+            }
+        })
+    })
+
+    if (isAuth) {
+        return (
+            <button
+                className="text-white text-[18px] font-[600] w-[41px] h-[41px] rounded-[6px] bg-[#0ca941] cursor-pointer hover:bg-[#0ecf4f]"
+            ><FontAwesomeIcon icon={faUser} />
+            </button>
+        )
+    } else {
+        return (
             <Link to="/auth" >
                 <button
                     className="text-white text-[18px] font-[600] w-[110px] h-[41px] rounded-[6px] bg-[#0ca941] cursor-pointer hover:bg-[#0ecf4f]"
                 >Login
                 </button>
             </Link>
-        </div>
-    );
+        );
+    }
 }
 
 export default AuthButton;
